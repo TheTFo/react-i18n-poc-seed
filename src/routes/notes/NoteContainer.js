4cf92ba3e9ce7040777ec5383f7545d00a2cef0e
Tony Foster <anthonyfoster85@gmail.com>
2017-01-27 15:02:17 -0500

    Added notes route

---
+++ src/routes/notes/NoteContainer.js
@@ -0,0 +1,5 @@
+import NoteForm from './components/NoteForm'
+
+// Not actually connecting this to a store
+export const NoteContainer = NoteForm;
+export default NoteContainer;
---
+++ src/routes/notes/components/NoteForm.js
@@ -0,0 +1,63 @@
+require('normalize.css/normalize.css');
+require('styles/App.css');
+
+import React from 'react';
+import NoteAdder from './NoteAdder';
+import NoteList from './NoteList';
+
+class NoteForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      noteText: '',
+      noteList: []
+    };
+
+    this.addNote = this.addNote.bind(this);
+    this.noteTextChanged = this.noteTextChanged.bind(this);
+    this.clearList = this.clearList.bind(this);
+  }
+
+  noteTextChanged(text) {
+    this.setState({ noteText: text });
+  }
+
+  addNote() {
+    if (this.state.noteText) {
+      this.setState({
+        noteList: this.state.noteList.concat([this.state.noteText]),
+        noteText: ''
+      });
+    }
+  }
+
+  clearList() {
+    this.setState({ noteList: [] });
+  }
+
+  render() {
+    return (
+      <div className="note-form">
+        <h3>Instructions: Put some text in the box and click "Add Note"</h3>
+        <NoteAdder
+          noteText={this.state.noteText}
+          onAddClick={this.addNote}
+          onNoteTextChange={this.noteTextChanged} />
+        <h2>
+          { this.state.noteList.length > 0 ?
+            `You have added ${this.state.noteList.length} notes.` :
+            'No notes have been added yet.'}
+        </h2>
+        <NoteList
+          onClickClear={this.clearList}
+          notes={this.state.noteList} />
+      </div>
+    );
+  }
+}
+
+NoteForm.defaultProps = {
+};
+
+export default NoteForm;
